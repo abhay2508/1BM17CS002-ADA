9c2742717c66f1a3471899733479784392990f92
abhay2508 <54144891+abhay2508@users.noreply.github.com>
2019-09-05 15:51:07 +0530

    Add files via upload

---
+++ islands.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#define ROW 50
+#define COL 50
+using namespace std;
+    int M[ROW][COL],n1,n2;
+    bool visited[ROW][COL];
+
+        void input(){
+        cout<<"enter the no. of rows \n";
+        cin>>n1;
+        cout<<"enter the no. of columns:";
+        cin>>n2;
+        for (int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+            visited[i][j]=false;
+        }
+        }
+        cout<<"enter the input matrix of graph\n";
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                cin>>M[i][j];
+            }}
+       }
+       int isSafe(int M[][COL],int row,int col,bool visited[][COL]){
+           return ((row>=0)&&(row<n1)&&(col>=0)&&(col<n2)&&(M[row][col]&& !visited[row][col]));
+       }
+       void DFS(int M[][COL],int row,int col,bool visited[][COL]){
+           static int rowNbr[]={-1,-1,-1,0,0,1,1,1};
+           static int colNbr[]={-1,0,1,-1,1,-1,0,1};
+           visited[row][col]=true;
+           for(int k=0;k<8;k++){
+            if(isSafe(M,row+rowNbr[k],col+colNbr[k],visited)){
+                DFS(M,row+rowNbr[k],col+colNbr[k],visited);
+            }
+           }
+       }
+       int countIslands(int M[][COL]){
+           int count=0;
+           for(int i=0;i<n1;++i){
+            for(int j=0;j<n2;++j){
+                if(M[i][j] && !visited[i][j]){
+                DFS(M,i,j,visited);
+                ++count;
+                }
+            }
+           }
+           return count;
+
+       }
+
+
+int main(){
+    input();
+    int n=countIslands(M);
+    cout<<n<<" islands are present";
+    return 0;
+}
+
